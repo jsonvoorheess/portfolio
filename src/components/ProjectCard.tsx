d1eddc5b68d84d22c9ee15285b4bb69bb0e8c9d1
broo <zarifullin17022005@icloud.com>
2025-02-12 02:30:50 +0300

    проекты

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,6 +1,6 @@
 import styles from './Portfolio.module.css';
 import ReadMoreIcon from '../assets/ReadMore.svg?react';
-
+import GithubIcon from "../assets/github_seeklogo.svg?react"
 
 interface ProjectCardProps {
   number: string;
@@ -9,9 +9,11 @@ interface ProjectCardProps {
   imageSrc: string;
   imagePosition: 'left' | 'right';
   websiteUrl: string;
+  githubUrl: string;
+  isDemo: boolean;
 }
 
-export function ProjectCard({ number, title, description, imageSrc, imagePosition, websiteUrl }: ProjectCardProps) {
+export function ProjectCard({ number, title, description, imageSrc, imagePosition, websiteUrl, githubUrl, isDemo }: ProjectCardProps) {
   const ImageSection = (
     <div className={imagePosition === 'right' ? styles.image2 : styles.image}>
       <img
@@ -33,9 +35,17 @@ export function ProjectCard({ number, title, description, imageSrc, imagePositio
         <p className={styles.imEvrenShahLoremIpsumIsSimplyDummyTextOfThePrintingAndTypesettingIndustryLoremIpsumHasBeenTheIndustrysStandardDummyTextEverSinceThe1500SWhenAnUnknownPrinterTookAGalleyOfTypeAndScrambledItToSpecimenBook}>
           {description}
         </p>
-        <a target="_blank" rel="noopener noreferrer" href={websiteUrl} aria-label="Read More" className={styles.readMore}>
+        <div className={styles.div2}>
+        {isDemo && <a target="_blank" rel="noopener noreferrer" href={websiteUrl} aria-label="Read More" className={styles.readMore}>
           <ReadMoreIcon />
+         
+        </a>}
+        <a target="_blank" rel="noopener noreferrer" href={githubUrl} aria-label="Read More" className={styles.readMore}>
+          <GithubIcon />
+         
         </a>
+        </div>
+        
       </div>
     </article>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,7 @@
 import styles from './Portfolio.module.css';
 import { ProjectCard } from './ProjectCard';
 import movieAppImage from '../assets/movieApp.png';
+import artAppImage from '../assets/artApp.png';
 
 export default function Projects() {
   return (
@@ -13,8 +14,10 @@ export default function Projects() {
       </header>
       
       <ProjectCard
-        number="num1"
-        title="Crypto Screener Application"
+        isDemo={true}
+        number=""
+        githubUrl="https://github.com/jsonvoorheess/movieApp"
+        title="Movie Reviewer"
         description="I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book."
         imageSrc={movieAppImage}
         imagePosition="left"
@@ -22,16 +25,20 @@ export default function Projects() {
       />
 
       <ProjectCard
-        number="num2"
-        title="Euphoria - Ecommerce (Apparels) Website Template"
+        isDemo={false}
+        number=""
+        githubUrl="https://github.com/jsonvoorheess/Articles-App"
+        title="Dev Clone"
         description="I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book. when an unknown printer took a galley of type and scrambled it to specimen book."
-        imageSrc="https://cdn.builder.io/api/v1/image/assets/dd15ee627bdf46a9b691a6ea1dddc9a1/76200726e63e16547e1c80dee9b699f85323ebb3a9506a92297a5570b58013a2?placeholderIfAbsent=true"
+        imageSrc={artAppImage}
         imagePosition="right"
         websiteUrl="https://movieappads.netlify.app/"
       />
 
       <ProjectCard
-        number="num3"
+        isDemo={false}
+        number=""
+        githubUrl="https://github.com/evrenshah/blog-website-template"
         title="Blog Website Template"
         description="I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book."
         imageSrc="https://cdn.builder.io/api/v1/image/assets/dd15ee627bdf46a9b691a6ea1dddc9a1/73a0742bd4cedd6ed245614ae5c3f037b8f0646dd23a6aaafd8a02519a5edd22?placeholderIfAbsent=true"
