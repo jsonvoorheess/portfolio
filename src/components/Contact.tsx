7bf4efce8311f37a3ec4344ddbce08a02a33a057
broo <zarifullin17022005@icloud.com>
2025-02-14 16:36:01 +0300

    final

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./Portfolio.module.css";
+import TeleIcon from "../assets/teleIcon.svg?react";
+import VkIcon from "../assets/vkIcon.svg?react";
 
 export const Contact: React.FC = () => {
   return (
     <div className={styles.contactSection}>
-      <div className={styles.contact}>Contact</div>
+      <div className={styles.contact}>Contacts</div>
       <div className={styles.contactText}>
         I'm currently looking to join a cross-functional team that values
         improving people's lives
@@ -12,7 +14,23 @@ export const Contact: React.FC = () => {
         through accessible design. or have a project in mind? Let's connect.
         <br />
         <br />
-        ibrhaimmemon930@gmail.com
+        <div className={styles.telegramDiv}>
+          <a
+            className={styles.telegramIcon}
+            href="https://t.me/jsvrhes"
+            target="_blank"
+          >
+            <TeleIcon />
+          </a>
+          <a
+            className={styles.telegramIcon}
+            href="https://vk.com/lyingtomyselfff"
+            target="_blank"
+          >
+            <VkIcon />
+          </a>
+        </div>
+        zarifullin1688@gmail.com
       </div>
     </div>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -19,7 +19,7 @@ export default function Projects() {
         number=""
         githubUrl="https://github.com/jsonvoorheess/movieApp"
         title="Movie Reviewer"
-        description="I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book."
+        description="A web application with a review of films and TV series with the ability to leave your favorite ones in the cart. There are two themes: light and dark. There is also a modal window with the ability to leave your email"
         imageSrc={movieAppImage}
         imagePosition="left"
         websiteUrl="https://movieappads.netlify.app/"
@@ -30,7 +30,7 @@ export default function Projects() {
         number=""
         githubUrl="https://github.com/jsonvoorheess/Articles-App"
         title="Dev Clone"
-        description="I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book. when an unknown printer took a galley of type and scrambled it to specimen book."
+        description="A simplified clone of the platform for developers with articles with the ability to leave comments and likes after successful OAuth GitHub authorization. There are also two topics on the site"
         imageSrc={artAppImage}
         imagePosition="right"
         websiteUrl="https://movieappads.netlify.app/"
@@ -39,9 +39,9 @@ export default function Projects() {
       <ProjectCard
         isDemo={false}
         number=""
-        githubUrl="https://github.com/evrenshah/blog-website-template"
-        title="Blog Website Template"
-        description="I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book."
+        githubUrl="https://github.com/jsonvoorheess/shop-app"
+        title="Сlothing store"
+        description="A web application that is a clothing store with the ability to log in using a login and password, after which you can add clothes to your cart"
         imageSrc={shopAppImage}
         imagePosition="left"
         websiteUrl="https://movieappads.netlify.app/"
